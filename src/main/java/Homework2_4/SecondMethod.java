51c6b501409e8528fc6068dc366c048a838a465c
123 <q556677@yandex.ru>
2022-06-11 23:45:45 +0400

    added two classes for handling Array

--- src/main/java/Homework2_4/SecondMethod.java
+++ src/main/java/Homework2_4/SecondMethod.java
@@ -1,14 +1,19 @@
 package Homework2_4;
 
 public class SecondMethod {
+    float[] tr1;
+    float[] tr2;
+
     public void method(float[] arr, int h) {
         long a = System.currentTimeMillis();
-        float[] tr1 = new float[h];
-        float[] tr2 = new float[h];
+        tr1 = new float[h];
+        tr2 = new float[h];
         System.arraycopy(arr, 0, tr1, 0, h);
         System.arraycopy(arr, h, tr2, 0, h);
         Thread thread1 = new Thread(new Tr1());
         thread1.start();
+        Thread thread2 = new Thread(new Tr2());
+        thread2.start();
 
 
         float[] mergedArr = new float[h * 2];
@@ -19,11 +24,20 @@ public class SecondMethod {
 
     class Tr1 implements Runnable{
         @Override
-        public void run(float[] arr) {
+        public void run() {
             for (int i = 0; i < tr1.length; i++) {
                 arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
 
     }
+    class Tr2 implements Runnable{
+        @Override
+        public void run() {
+            for (int i = 0; i < tr1.length; i++) {
+                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+    }
+
 
 }
