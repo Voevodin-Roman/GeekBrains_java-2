2163dc37462dfcb177a56f413a97f9d9c3ce8c32
123 <q556677@yandex.ru>
2022-06-12 11:40:31 +0400

    add Homework2_5

--- src/main/java/Homework2_4/FirstMethod.java
+++ src/main/java/Homework2_4/FirstMethod.java
@@ -6,6 +6,6 @@ public class FirstMethod {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("Время обработки массива одним потоком: " + (System.currentTimeMillis() - a));
     }
 }
--- src/main/java/Homework2_4/SecondMethod.java
+++ src/main/java/Homework2_4/SecondMethod.java
@@ -3,40 +3,42 @@ package Homework2_4;
 public class SecondMethod {
     float[] tr1;
     float[] tr2;
-
+    //Метод обрабатывает входящий массив, разделяет его на 2 части и просчитывает двумя потоками. Потом склеивает части назад.
     public void method(float[] arr, int h) {
         long a = System.currentTimeMillis();
         tr1 = new float[h];
         tr2 = new float[h];
         System.arraycopy(arr, 0, tr1, 0, h);
         System.arraycopy(arr, h, tr2, 0, h);
-        Thread thread1 = new Thread(new Tr1());
-        Thread thread2 = new Thread(new Tr2());
+        TrArr trArr1 = new TrArr(tr1);
+        TrArr trArr2 = new TrArr(tr2);
+        Thread thread1 = new Thread(trArr1);
+        Thread thread2 = new Thread(trArr2);
         thread1.start();
         thread2.start();
         float[] mergedArr = new float[h * 2];
-        System.arraycopy(tr1, 0, mergedArr, 0, h);
-        System.arraycopy(tr2, 0, mergedArr, h, h);
-        System.out.println(System.currentTimeMillis() - a);
+        System.arraycopy(trArr1.getTr(), 0, mergedArr, 0, h);
+        System.arraycopy(trArr2.getTr(), 0, mergedArr, h, h);
+        System.out.println("Время обработки массива двумя потоками: " + (System.currentTimeMillis() - a));
     }
 
-    class Tr1 implements Runnable{
+    public class TrArr implements Runnable{
+       private final float[] tr;
+
+        public TrArr(float[] tr) {
+            this.tr = tr;
+        }
+
+        public float[] getTr() {
+            return tr;
+        }
+
         @Override
         public void run() {
             for (int i = 0; i < tr1.length; i++) {
-                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                tr[i] = (float) (tr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
         }
-
     }
-    class Tr2 implements Runnable{
-        @Override
-        public void run() {
-            for (int i = 0; i < tr1.length; i++) {
-                arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        }
-
-    }
-
 
 }
